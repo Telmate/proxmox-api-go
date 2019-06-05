5946614fd137952be0425dfe6207c5d45e1df02d
Andreas Gruhler <andreas.gruhler@adfinis-sygroup.ch>
2019-06-05 20:18:58 +0200

    add parameters for /nodes/{n}/lxc

--- proxmox/config_lxc.go
+++ proxmox/config_lxc.go
@@ -8,29 +8,86 @@ import (
 	"strings"
 )
 
-// ConfigLxc - Proxmox API LXC options
-type ConfigLxc struct {
-	Ostemplate      string       `json:"ostemplate"`
-	Storage         string       `json:"storage"`
-	Pool            string       `json:"pool"`
-	Password        string       `json:"password"`
-	Hostname        string       `json:"hostname"`
-	Networks        QemuDevices  `json:"networks"`
+type (
+	LxcMountpoints  map[int]map[string]interface{}
+	LxcUnused       map[int]map[string]interface{}
+	LxcDevice       map[string]interface{}
+	LxcDeviceParam  []string
+)
+
+// LXC options for the Proxmox API
+type configLxc struct {
+	Ostemplate         string         `json:"ostemplate"`
+        Arch               string         `json:"arch"`
+        BWLimit            int            `json:"bwlimit,omitempty"`
+        CMode              string         `json:"cmode"`
+        Console            bool           `json:"console"`
+        Cores              int            `json:"cores,omitempty"`
+        CPULimit           int            `json:"cpulimit"`
+        CPUUnits           int            `json:"cpuunits"`
+        Description        string         `json:"description,omitempty"`
+        Features           string         `json:"features,omitempty"`
+        Force              bool           `json:"force,omitempty"`
+        Hookscript         string         `json:"hookscript,omitempty"`
+	Hostname           string         `json:"hostname,omitempty"`
+        IgnoreUnpackErrors bool           `json:"ignore-unpack-errors,omitempty"`
+	Lock               string         `json:"lock,omitempty"`
+        Memory             int            `json:"memory"`
+        Mountpoints        LxcMountpoints `json:"mountpoints,omitempty"`
+        Nameserver         string         `json:"nameserver,omitempty"`
+	Networks           QemuDevices    `json:"networks,omitempty"`
+        OnBoot             bool           `json:"onboot"`
+        OsType             string         `json:"ostype,omitempty"`
+	Password           string         `json:"password,omitempty"`
+	Pool               string         `json:"pool,omitempty"`
+        Protection         bool           `json:"protection"`
+        Restore            bool           `json:"restore,omitempty"`
+	RootFs             string         `json:"rootfs,omitempty"`
+	SearchDomain       string         `json:"searchdomain,omitempty"`
+	SSHPublicKeys      string         `json:"ssh-public-keys,omitempty"`
+        Start              bool           `json:"start"`
+	Startup            string         `json:"startup,omitempty"`
+	Storage            string         `json:"storage"`
+        Swap               int            `json:"swap"`
+	Template           bool           `json:"template,omitempty"`
+        Tty                int            `json:"tty"`
+	Unique             bool           `json:"unique,omitempty"`
+	Unprivileged       bool           `json:"unprivileged"`
+	Unused             LxcUnused      `json:"unused,omitempty"`
 }
 
-func NewConfigLxcFromJson(io io.Reader) (config *ConfigLxc, err error) {
-	config = &ConfigLxc{}
+func NewConfigLxc() (configLxc) {
+	return configLxc{
+		Arch: "amd64",
+		CMode: "tty",
+		Console: true,
+		CPULimit: 0,
+		CPUUnits: 1024,
+		Memory: 512,
+		OnBoot: false,
+		Protection: false,
+		Start: false,
+		Storage: "local",
+		Swap: 512,
+		Template: false,
+		Tty: 2,
+		Unprivileged: false,
+	}
+}
+
+func NewConfigLxcFromJson(io io.Reader) (config configLxc, err error) {
+	config = NewConfigLxc()
 	err = json.NewDecoder(io).Decode(config)
 	if err != nil {
 		log.Fatal(err)
-		return nil, err
+		return config, err
 	}
 	log.Println(config)
 	return
 }
 
 // create LXC container using the Proxmox API
-func (config ConfigLxc) CreateLxc(vmr *VmRef, client *Client) (err error) {
+func (config configLxc) CreateLxc(vmr *VmRef, client *Client) (err error) {
 	vmr.SetVmType("lxc")
 
         // convert config to map
