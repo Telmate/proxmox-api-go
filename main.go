cd2f863b22edf2c460c286e30356be48ef1616ef
Virgil <virgil@paranoiaque.fr>
2019-08-02 21:45:20 +0200

    Useless var

--- main.go
+++ main.go
@@ -23,8 +23,7 @@ func main() {
 		tlsconf = nil
 	}
 	c, _ := proxmox.NewClient(os.Getenv("PM_API_URL"), nil, tlsconf)
-	otp := os.Getenv("PM_OTP")
-	err := c.Login(os.Getenv("PM_USER"), os.Getenv("PM_PASS"), otp)
+	err := c.Login(os.Getenv("PM_USER"), os.Getenv("PM_PASS"), os.Getenv("PM_OTP"))
 	if err != nil {
 		log.Fatal(err)
 	}
