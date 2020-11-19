a3425019af82d0743868d31e7cdf06e16576c8e2
yukron <yukron@gmail.com>
2020-11-19 14:00:58 +0300

    fixed storage_type & format for disk (#100)

--- proxmox/config_qemu.go
+++ proxmox/config_qemu.go
@@ -598,8 +598,14 @@ func NewConfigQemuFromApi(vmr *VmRef, client *Client) (config *ConfigQemu, err e
 		diskConfMap["storage"] = storageName
 		diskConfMap["file"] = fileName
 
-		storageContent, err := client.GetStorageContent(vmr, storageName)
+		filePath := diskConfMap["volume"]
 
+		// Get disk format
+		storageContent, err := client.GetStorageContent(vmr, storageName)
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
 		var storageFormat string
 		contents := storageContent["data"].([]interface{})
 		for content := range contents {
@@ -609,7 +615,9 @@ func NewConfigQemuFromApi(vmr *VmRef, client *Client) (config *ConfigQemu, err e
 				break
 			}
 		}
+		diskConfMap["format"] = storageFormat
 
+		// Get storage type for disk
 		var storageStatus map[string]interface{}
 		storageStatus, err = client.GetStorageStatus(vmr, storageName)
 		if err != nil {
@@ -619,7 +627,6 @@ func NewConfigQemuFromApi(vmr *VmRef, client *Client) (config *ConfigQemu, err e
 		storageType := storageStatus["type"]
 
 		diskConfMap["storage_type"] = storageType
-		diskConfMap["format"] = storageFormat
 
 		// Convert to gigabytes if disk size was received in terabytes
 		sizeIsInTerabytes, err := regexp.MatchString("[0-9]+T", diskConfMap["size"].(string))
