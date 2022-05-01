804252bf9ebf8ea0b2cc818bdb8b5a3ec609d653
Tinyblargon <tinyblargon@tiny-team.net>
2022-05-02 01:54:52 +0200

    Intergration Test: Pool

--- test/cli/shared_tests.go
+++ test/cli/shared_tests.go
@@ -10,6 +10,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type Test struct {
+	Name string
+	Expected string
+	NotExpected string
+	ReqErr bool
+	Contains bool
+	NotContains bool
+	Args []string
+}
 
 func ListTest(t *testing.T, args []string, expected string) {
 	cli.RootCmd.SetArgs(append(args))
@@ -22,4 +31,34 @@ func ListTest(t *testing.T, args []string, expected string) {
 
 	out, _ := ioutil.ReadAll(buffer)
 	assert.Contains(t, string(out), expected)
+}
+
+func (test *Test) StandardTest(t *testing.T) {
+	SetEnvironmentVariables()
+	cli.RootCmd.SetArgs(test.Args)
+	buffer := new(bytes.Buffer)
+	cli.RootCmd.SetOut(buffer)
+	err := cli.RootCmd.Execute()
+
+	if test.ReqErr {
+		require.Error(t, err)
+	} else {
+		require.NoError(t, err)
+	}
+	if test.Expected != "" {
+		out, _ := ioutil.ReadAll(buffer)
+		if test.Contains {
+			assert.Contains(t, string(out), test.Expected)
+		} else {
+			assert.Equal(t, string(out), test.Expected)
+		}
+	}
+	if test.NotExpected != "" {
+		out, _ := ioutil.ReadAll(buffer)
+		if test.NotContains {
+			assert.NotContains(t, string(out), test.NotExpected)
+		} else {
+			assert.NotEqual(t, string(out), test.NotExpected)
+		}
+	}
 }
