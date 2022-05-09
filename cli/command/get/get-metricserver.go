0292b0e38e2b24b3bb29ba55bb0352f7e62287b4
Tinyblargon <tinyblargon@tiny-team.net>
2022-05-09 11:36:31 +0200

    Add: get metricserver Command

---
+++ cli/command/get/get-metricserver.go
@@ -0,0 +1,17 @@
+package get
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var get_metricserverCmd = &cobra.Command{
+	Use:   "metricserver METRICSID",
+	Short: "Gets the configuration of the specified MetricServer",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return GetConfig(args, "MetricServer")
+	},
+}
+
+func init() {
+	getCmd.AddCommand(get_metricserverCmd)
+}
