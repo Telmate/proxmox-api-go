b8fd039107d6596a9731332c70448ead4f273257
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2022-09-13 18:49:18 +0200

    Add qemu specific guest commands

---
+++ cli/command/guest/qemu/guest-qemu-reset.go
@@ -0,0 +1,25 @@
+package qemu
+
+import (
+	"github.com/Telmate/proxmox-api-go/cli"
+	"github.com/Telmate/proxmox-api-go/proxmox"
+	"github.com/spf13/cobra"
+)
+
+var qemu_resetCmd = &cobra.Command{
+	Use:   "reset GUESTID",
+	Short: "Resets the speciefid guest",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		vmr := proxmox.NewVmRef(cli.ValidateIntIDset(args, "GuestID"))
+		c := cli.NewClient()
+		_, err = c.StartVm(vmr)
+		if err == nil {
+			cli.PrintGuestStatus(qemuCmd.OutOrStdout(), vmr.VmId(), "reset")
+		}
+		return
+	},
+}
+
+func init() {
+	qemuCmd.AddCommand(qemu_resetCmd)
+}
