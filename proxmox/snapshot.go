6551610be6b7ffbf261c57189364c09bee046f20
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2022-10-22 12:15:51 +0000

    feat: Add function to format the snapshots as a json tree

--- proxmox/snapshot.go
+++ proxmox/snapshot.go
@@ -28,3 +28,49 @@ func (config *ConfigSnapshot) CreateSnapshot(guestId uint, c *Client) (err error
 	}
 	return
 }
+
+type snapshot struct {
+	Name        string      `json:"name"`
+	SnapTime    uint        `json:"time,omitempty"`
+	Description string      `json:"description,omitempty"`
+	VmState     bool        `json:"ram,omitempty"`
+	Children    []*snapshot `json:"children,omitempty"`
+	Parent      string      `json:"-"`
+}
+
+func FormatSnapshotsTree(taskResponse []interface{}) (tree *snapshot) {
+	snapshotList := make([]*snapshot, len(taskResponse))
+	for i, e := range taskResponse {
+		snapshotList[i] = &snapshot{}
+		if _, isSet := e.(map[string]interface{})["description"]; isSet {
+			snapshotList[i].Description = e.(map[string]interface{})["description"].(string)
+		}
+		if _, isSet := e.(map[string]interface{})["name"]; isSet {
+			snapshotList[i].Name = e.(map[string]interface{})["name"].(string)
+		}
+		if _, isSet := e.(map[string]interface{})["parent"]; isSet {
+			snapshotList[i].Parent = e.(map[string]interface{})["parent"].(string)
+		}
+		if _, isSet := e.(map[string]interface{})["snaptime"]; isSet {
+			snapshotList[i].SnapTime = uint(e.(map[string]interface{})["snaptime"].(float64))
+		}
+		if _, isSet := e.(map[string]interface{})["vmstate"]; isSet {
+			snapshotList[i].VmState = Itob(int(e.(map[string]interface{})["vmstate"].(float64)))
+		}
+	}
+	for _, e := range snapshotList {
+		for _, ee := range snapshotList {
+			if e.Parent == ee.Name {
+				ee.Children = append(ee.Children, e)
+				break
+			}
+		}
+	}
+	for _, e := range snapshotList {
+		if e.Parent == "" {
+			tree = e
+			break
+		}
+	}
+	return
+}
