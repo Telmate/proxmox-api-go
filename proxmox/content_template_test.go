7ac592dfb92e0539859e7360afbdfe96036def54
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-01-08 13:50:00 +0000

    test: check if template list is properly mapped to struct from api values

--- proxmox/content_template_test.go
+++ proxmox/content_template_test.go
@@ -65,3 +65,102 @@ func Test_ConfigContent_Template_Validate(t *testing.T) {
 		require.Equal(t, e.output, e.input.Validate())
 	}
 }
+
+func Test_createTemplateList(t *testing.T) {
+	testData := []struct {
+		Input  []interface{}
+		Output *[]TemplateItem
+	}{
+		{
+			Input: []interface{}{
+				map[string]interface{}{
+					"location":    "http://mirror.turnkeylinux.org/turnkeylinux/images/proxmox/debian-11-turnkey-drupal7_17.1-1_amd64.tar.gz",
+					"manageurl":   "http://__IPADDRESS__/",
+					"description": "TurnKey Drupal 7 - Content Management Framework",
+					"version":     "17.1-1",
+					"template":    "debian-11-turnkey-drupal7_17.1-1_amd64.tar.gz",
+					"headline":    "TurnKey Drupal 7",
+					"source":      "https://releases.turnkeylinux.org/pve",
+					"package":     "turnkey-drupal7",
+					"section":     "turnkeylinux",
+				},
+				map[string]interface{}{
+					"infopage":     "http://www.turnkeylinux.org/mumble",
+					"section":      "turnkeylinux",
+					"headline":     "TurnKey Mumble",
+					"manageurl":    "http://__IPADDRESS__/",
+					"sha512sum":    "30a81eec66968817b9e06502141a476d0707643e3649034f0edc69692fcbff6ccde49e43ec9ef6ee09e2655faab1202e915428cc30e77af30749dabbac944d28",
+					"type":         "lxc",
+					"architecture": "amd64",
+					"os":           "debian-11",
+					"template":     "debian-11-turnkey-mumble_17.1-1_amd64.tar.gz",
+				},
+				map[string]interface{}{
+					"location":     "http://mirror.turnkeylinux.org/turnkeylinux/images/proxmox/debian-11-turnkey-mattermost_17.1-1_amd64.tar.gz",
+					"type":         "lxc",
+					"os":           "debian-11",
+					"package":      "turnkey-mattermost",
+					"sha512sum":    "5b6d3cdd8fac06494cd094dac36a9b2820889c4b3e0ef033a17b3f4d82d12390fccbcb0ab1425472595d163c6ce28c047bda5bd8e4e9c551a7db2744ada3c130",
+					"architecture": "amd64",
+					"description":  "TurnKey Mattermost - Open Source, self-hosted Slack-alternative",
+					"version":      "17.1-1",
+					"infopage":     "http://www.turnkeylinux.org/mattermost",
+					"source":       "https://releases.turnkeylinux.org/pve",
+				},
+			},
+			Output: &[]TemplateItem{
+				{
+					Architecture:   "",
+					Description:    "TurnKey Drupal 7 - Content Management Framework",
+					Headline:       "TurnKey Drupal 7",
+					InfoPage:       "",
+					Location:       "http://mirror.turnkeylinux.org/turnkeylinux/images/proxmox/debian-11-turnkey-drupal7_17.1-1_amd64.tar.gz",
+					ManageURL:      "http://__IPADDRESS__/",
+					OS:             "",
+					Package:        "turnkey-drupal7",
+					Section:        "turnkeylinux",
+					SHA512Checksum: "",
+					Source:         "https://releases.turnkeylinux.org/pve",
+					Template:       "debian-11-turnkey-drupal7_17.1-1_amd64.tar.gz",
+					Type:           "",
+					Version:        "17.1-1",
+				},
+				{
+					Architecture:   "amd64",
+					Description:    "",
+					Headline:       "TurnKey Mumble",
+					InfoPage:       "http://www.turnkeylinux.org/mumble",
+					Location:       "",
+					ManageURL:      "http://__IPADDRESS__/",
+					OS:             "debian-11",
+					Package:        "",
+					Section:        "turnkeylinux",
+					SHA512Checksum: "30a81eec66968817b9e06502141a476d0707643e3649034f0edc69692fcbff6ccde49e43ec9ef6ee09e2655faab1202e915428cc30e77af30749dabbac944d28",
+					Source:         "",
+					Template:       "debian-11-turnkey-mumble_17.1-1_amd64.tar.gz",
+					Type:           "lxc",
+					Version:        "",
+				},
+				{
+					Architecture:   "amd64",
+					Description:    "TurnKey Mattermost - Open Source, self-hosted Slack-alternative",
+					Headline:       "",
+					InfoPage:       "http://www.turnkeylinux.org/mattermost",
+					Location:       "http://mirror.turnkeylinux.org/turnkeylinux/images/proxmox/debian-11-turnkey-mattermost_17.1-1_amd64.tar.gz",
+					ManageURL:      "",
+					OS:             "debian-11",
+					Package:        "turnkey-mattermost",
+					Section:        "",
+					SHA512Checksum: "5b6d3cdd8fac06494cd094dac36a9b2820889c4b3e0ef033a17b3f4d82d12390fccbcb0ab1425472595d163c6ce28c047bda5bd8e4e9c551a7db2744ada3c130",
+					Source:         "https://releases.turnkeylinux.org/pve",
+					Template:       "",
+					Type:           "lxc",
+					Version:        "17.1-1",
+				},
+			},
+		},
+	}
+	for _, e := range testData {
+		require.Equal(t, e.Output, createTemplateList(e.Input))
+	}
+}
