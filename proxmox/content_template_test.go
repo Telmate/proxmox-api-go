9895c804a40107927e017c41e17eb9fd58d94619
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-01-08 13:41:32 +0000

    test: add test for validation func

--- proxmox/content_template_test.go
+++ proxmox/content_template_test.go
@@ -1,10 +1,15 @@
 package proxmox
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func Test_ConfigContent_Template_error(t *testing.T) {
+	require.Equal(t, errors.New("the value of (Node) may not be empty"), ConfigContent_Template{}.error("Node"))
+}
 
 func Test_ConfigContent_Template_mapToApiValues(t *testing.T) {
 	testData := []struct {
@@ -24,5 +29,39 @@ func Test_ConfigContent_Template_mapToApiValues(t *testing.T) {
 	}
 	for _, e := range testData {
 		require.Equal(t, e.output, e.input.mapToApiValues())
+	}
+}
+
+func Test_ConfigContent_Template_Validate(t *testing.T) {
+	testData := []struct {
+		input  ConfigContent_Template
+		output error
+	}{
+		{
+			input:  ConfigContent_Template{},
+			output: ConfigContent_Template{}.error("Node"),
+		},
+		{
+			input:  ConfigContent_Template{Node: "notEmpty"},
+			output: ConfigContent_Template{}.error("Storage"),
+		},
+		{
+			input: ConfigContent_Template{
+				Node:    "notEmpty",
+				Storage: "notEmpty",
+			},
+			output: ConfigContent_Template{}.error("Template"),
+		},
+		{
+			input: ConfigContent_Template{
+				Node:     "notEmpty",
+				Storage:  "notEmpty",
+				Template: "notEmpty",
+			},
+			output: nil,
+		},
+	}
+	for _, e := range testData {
+		require.Equal(t, e.output, e.input.Validate())
 	}
 }
