564f196059fe7a0616858238e90e89a956cc373a
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-01-08 13:40:25 +0000

    feat: add validation function for ConfigContent_Template

--- proxmox/content_template.go
+++ proxmox/content_template.go
@@ -1,9 +1,17 @@
 package proxmox
 
+import (
+	"errors"
+)
+
 type ConfigContent_Template struct {
 	Node     string
 	Storage  string
 	Template string
+}
+
+func (content ConfigContent_Template) error(text string) error {
+	return errors.New("the value of (" + text + ") may not be empty")
 }
 
 func (content ConfigContent_Template) mapToApiValues() map[string]interface{} {
@@ -11,6 +19,19 @@ func (content ConfigContent_Template) mapToApiValues() map[string]interface{} {
 		"storage":  content.Storage,
 		"template": content.Template,
 	}
+}
+
+func (content ConfigContent_Template) Validate() (err error) {
+	if content.Node == "" {
+		return content.error("Node")
+	}
+	if content.Storage == "" {
+		return content.error("Storage")
+	}
+	if content.Template == "" {
+		return content.error("Template")
+	}
+	return
 }
 
 func DownloadLxcTemplate(client *Client, content ConfigContent_Template) (err error) {
