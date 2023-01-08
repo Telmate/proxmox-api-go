cef5df6226f11d79d65e495dc2b744e049696dda
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-01-08 13:47:13 +0000

    feat: List all LXC Templates available for download

--- proxmox/content_template.go
+++ proxmox/content_template.go
@@ -34,7 +34,85 @@ func (content ConfigContent_Template) Validate() (err error) {
 	return
 }
 
+type TemplateItem struct {
+	Architecture   string
+	Description    string
+	Headline       string
+	InfoPage       string
+	Location       string
+	ManageURL      string
+	OS             string
+	Package        string
+	Section        string
+	SHA512Checksum string
+	Source         string
+	Template       string
+	Type           string
+	Version        string
+}
+
+func createTemplateList(templateList []interface{}) *[]TemplateItem {
+	templates := make([]TemplateItem, len(templateList))
+	for i := range templateList {
+		itemMap := templateList[i].(map[string]interface{})
+		tmp := TemplateItem{}
+		if _, isSet := itemMap["architecture"]; isSet {
+			tmp.Architecture = itemMap["architecture"].(string)
+		}
+		if _, isSet := itemMap["description"]; isSet {
+			tmp.Description = itemMap["description"].(string)
+		}
+		if _, isSet := itemMap["headline"]; isSet {
+			tmp.Headline = itemMap["headline"].(string)
+		}
+		if _, isSet := itemMap["infopage"]; isSet {
+			tmp.InfoPage = itemMap["infopage"].(string)
+		}
+		if _, isSet := itemMap["location"]; isSet {
+			tmp.Location = itemMap["location"].(string)
+		}
+		if _, isSet := itemMap["manageurl"]; isSet {
+			tmp.ManageURL = itemMap["manageurl"].(string)
+		}
+		if _, isSet := itemMap["os"]; isSet {
+			tmp.OS = itemMap["os"].(string)
+		}
+		if _, isSet := itemMap["package"]; isSet {
+			tmp.Package = itemMap["package"].(string)
+		}
+		if _, isSet := itemMap["section"]; isSet {
+			tmp.Section = itemMap["section"].(string)
+		}
+		if _, isSet := itemMap["sha512sum"]; isSet {
+			tmp.SHA512Checksum = itemMap["sha512sum"].(string)
+		}
+		if _, isSet := itemMap["source"]; isSet {
+			tmp.Source = itemMap["source"].(string)
+		}
+		if _, isSet := itemMap["template"]; isSet {
+			tmp.Template = itemMap["template"].(string)
+		}
+		if _, isSet := itemMap["type"]; isSet {
+			tmp.Type = itemMap["type"].(string)
+		}
+		if _, isSet := itemMap["version"]; isSet {
+			tmp.Version = itemMap["version"].(string)
+		}
+		templates[i] = tmp
+	}
+	return &templates
+}
+
 func DownloadLxcTemplate(client *Client, content ConfigContent_Template) (err error) {
 	_, err = client.PostWithTask(content.mapToApiValues(), "/nodes/"+content.Node+"/aplinfo")
+	return
+}
+
+func ListTemplates(client *Client, node string) (templateList *[]TemplateItem, err error) {
+	tmpList, err := client.GetItemListInterfaceArray("/nodes/" + node + "/aplinfo")
+	if err != nil {
+		return
+	}
+	templateList = createTemplateList(tmpList)
 	return
 }
