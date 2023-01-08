74577d80ad12b64dd4f259072df6c965069d2015
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2023-01-08 14:01:10 +0000

    feat: add CLI command to list files in a Storage

---
+++ cli/command/list/list-files.go
@@ -0,0 +1,26 @@
+package list
+
+import (
+	"github.com/Telmate/proxmox-api-go/cli"
+	"github.com/Telmate/proxmox-api-go/proxmox"
+	"github.com/spf13/cobra"
+)
+
+var list_FileCmd = &cobra.Command{
+	Use:   "files NODE STORAGE TYPE",
+	Short: "Prints a list of Files of the specified type in raw json format",
+	Args:  cobra.ExactArgs(3),
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		c := cli.NewClient()
+		templates, err := proxmox.ListFiles(c, args[0], args[1], proxmox.ContentType(args[2]))
+		if err != nil {
+			return
+		}
+		cli.PrintRawJson(listCmd.OutOrStdout(), templates)
+		return
+	},
+}
+
+func init() {
+	listCmd.AddCommand(list_FileCmd)
+}
