0a6089b0685d265937cac96272d7688b463877f5
Richard Franks <git@rf152.co.uk>
2023-04-05 20:55:14 +0100

    Fixed err never used

--- test/api/Authentication/authentication_apikey_test.go
+++ test/api/Authentication/authentication_apikey_test.go
@@ -16,13 +16,13 @@ func Test_Root_Login_Correct_Api_Key(t *testing.T) {
 
 	token := pxapi.ApiToken{TokenId: "testing", Comment: "This is a test", Expire: 0, Privsep: false}
 
-	value, err := user.CreateApiToken(Test.GetClient(), token)
+	value, _ := user.CreateApiToken(Test.GetClient(), token)
 
 	NewTest := api_test.Test{}
 	NewTest.CreateClient()
 	NewTest.GetClient().SetAPIToken("root@pam!testing", value)
 
-	_, err = NewTest.GetClient().GetVersion()
+	_, err := NewTest.GetClient().GetVersion()
 	require.NoError(t, err)
 
 	user.DeleteApiToken(Test.GetClient(), token)
