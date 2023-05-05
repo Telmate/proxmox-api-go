517592649f5de5d65dbae462c8eef50371a3de14
Richard Franks <git@rf152.co.uk>
2023-05-05 09:13:49 +0100

    Removed unused function

--- test/api/Test.go
+++ test/api/Test.go
@@ -2,6 +2,7 @@ package api_test
 
 import (
 	"crypto/tls"
+
 	pxapi "github.com/Telmate/proxmox-api-go/proxmox"
 )
 
@@ -13,12 +14,12 @@ type Test struct {
 	HttpHeaders string
 	RequireSSL  bool
 
-	_client     *pxapi.Client
+	_client *pxapi.Client
 }
 
 func (test *Test) CreateClient() (err error) {
 	if test.APIurl == "" {
-		test.APIurl = "https://127.0.0.1:8006/api2/json"
+		test.APIurl = "https://172.26.8.219:8006/api2/json"
 	}
 	if test.UserID == "" {
 		test.UserID = "root@pam"
@@ -39,7 +40,6 @@ func (test *Test) CreateClient() (err error) {
 func (test *Test) GetClient() (client *pxapi.Client) {
 	return test._client
 }
-
 
 func (test *Test) Login() (err error) {
 	if test._client == nil {
