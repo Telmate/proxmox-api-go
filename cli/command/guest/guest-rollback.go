8c93ee1ec56e622b0247d0755319873a8e6e026d
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2024-02-29 20:01:45 +0100

    fix: incorrect types

--- cli/command/guest/guest-rollback.go
+++ cli/command/guest/guest-rollback.go
@@ -15,7 +15,7 @@ var guest_rollbackCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
 		vmr := proxmox.NewVmRef(cli.ValidateIntIDset(args, "GuestID"))
 		snapName := cli.RequiredIDset(args, 1, "SnapshotName")
-		_, err = proxmox.RollbackSnapshot(cli.NewClient(), vmr, snapName)
+		_, err = proxmox.RollbackSnapshot(cli.NewClient(), vmr, proxmox.SnapshotName(snapName))
 		if err == nil {
 			fmt.Fprintf(GuestCmd.OutOrStdout(), "Guest with id (%d) has been rolled back to snapshot (%s)\n", vmr.VmId(), snapName)
 		}
