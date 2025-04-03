08ab6ac2496b49e2c914681856d2d88843b6be3f
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2025-04-03 22:57:51 +0200

    test: task cleanup on error

--- proxmox/task_test.go
+++ proxmox/task_test.go
@@ -2,6 +2,7 @@ package proxmox
 
 import (
 	"context"
+	"errors"
 	"runtime"
 	"testing"
 	"time"
@@ -9,6 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// Test if the status is returned correctly.
 func Test_task_Status(t *testing.T) {
 	initialGoroutines := runtime.NumGoroutine()
 	statusChannel := make(chan func() (map[string]interface{}, error))
@@ -59,6 +61,7 @@ func Test_task_Status(t *testing.T) {
 	cancel()
 }
 
+// Test if the log is returned correctly.
 func Test_task_Logs(t *testing.T) {
 	initialGoroutines := runtime.NumGoroutine()
 	statusChannel := make(chan func() (map[string]interface{}, error))
@@ -143,6 +146,83 @@ func Test_task_Logs(t *testing.T) {
 	ended, err := task.Ended()
 	require.True(t, ended)
 	require.NoError(t, err)
+
+	finalGoroutines := runtime.NumGoroutine()
+	if finalGoroutines > initialGoroutines {
+		t.Errorf("Potential goroutine leak: initial = %d, final = %d", initialGoroutines, finalGoroutines)
+	} else {
+		t.Log("No goroutine leaks detected")
+	}
+	cancel()
+}
+
+// Test if all go routines are cleaned up after an error occurs.
+func Test_task_Log_error(t *testing.T) {
+	initialGoroutines := runtime.NumGoroutine()
+	statusChannel := make(chan func() (map[string]interface{}, error))
+	logChannel := make(chan func() (map[string]interface{}, error))
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &mocClient{
+		getItemConfigFunc: func(ctx context.Context, url, text, message string, errorStrings []string) (map[string]interface{}, error) {
+			// Block until data is sent into the channel
+			select {
+			case f := <-statusChannel:
+				return f()
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			}
+		},
+		getItemListFunc: func(ctx context.Context, url string) (map[string]interface{}, error) {
+			// Block until data is sent into the channel
+			select {
+			case f := <-logChannel:
+				return f()
+			case <-ctx.Done():
+				return nil, ctx.Err()
+			}
+		},
+	}
+	task := newTask(ctx, c, "UPID:pve-test:002860A9:051E01C1:67536165:qmmove:102:root@pam:", 10*time.Millisecond)
+
+	go func() {
+		statusChannel <- func() (map[string]interface{}, error) {
+			return map[string]interface{}{
+				"data": map[string]interface{}{
+					"status": "running"}}, nil
+		}
+		logChannel <- func() (map[string]interface{}, error) {
+			return map[string]interface{}{
+				"total": float64(4),
+				"data": []interface{}{
+					map[string]interface{}{"t": "1"},
+					map[string]interface{}{"t": "2"},
+					map[string]interface{}{"t": "3"},
+					map[string]interface{}{"t": "4"},
+				}}, nil
+		}
+	}()
+	time.Sleep(50 * time.Millisecond)
+	require.Equal(t, "running", task.Status())
+	require.Equal(t, []string{
+		"1",
+		"2",
+		"3",
+		"4",
+	}, task.Log())
+
+	go func() {
+		statusChannel <- func() (map[string]interface{}, error) {
+			return map[string]interface{}{
+					"data": map[string]interface{}{
+						"status":             "stopped",
+						taskApiKeyExitStatus: "ERROR"}},
+				errors.New("error status")
+		}
+	}()
+	time.Sleep(50 * time.Millisecond)
+	ended, err := task.Ended()
+	require.True(t, ended)
+	require.Error(t, err)
 
 	finalGoroutines := runtime.NumGoroutine()
 	if finalGoroutines > initialGoroutines {
