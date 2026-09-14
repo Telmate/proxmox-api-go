a7053bcb9a13a15a5663e0faef87ff8febfab759
Tinyblargon <76069640+Tinyblargon@users.noreply.github.com>
2026-09-14 21:19:05 +0200

    perf: optimize memory layout

--- proxmox/client.go
+++ proxmox/client.go
@@ -75,12 +75,12 @@ const (
 // VmRef - virtual machine ref parts
 // map[type:qemu node:proxmox1-xx id:qemu/132 diskread:5.57424738e+08 disk:0 netin:5.9297450593e+10 mem:3.3235968e+09 uptime:1.4567097e+07 vmid:132 template:0 maxcpu:2 netout:6.053310416e+09 maxdisk:3.4359738368e+10 maxmem:8.592031744e+09 diskwrite:1.49663619584e+12 status:running cpu:0.00386980694947209 name:appt-app1-dev.xxx.xx]
 type VmRef struct { // In the future this will be replaced by GuestRef and guestRef.
-	vmId    GuestID
+	haGroup string
+	haState string
 	node    NodeName
 	pool    PoolName
+	vmId    GuestID
 	vmType  GuestType
-	haState string
-	haGroup string
 }
 
 func (vmr *VmRef) SetNode(node string) {
